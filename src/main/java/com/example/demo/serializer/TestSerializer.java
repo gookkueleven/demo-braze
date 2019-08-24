59d2c59a9fe4373fb740c31f92adc6914727c803
Eluzy Demo <eluzy.demo@gmail.com>
2019-08-24 22:17:28 +0700

    change currency to use application.yml

--- src/main/java/com/example/demo/serializer/TestSerializer.java
+++ src/main/java/com/example/demo/serializer/TestSerializer.java
@@ -3,19 +3,23 @@ package com.example.demo.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
-@Component
+@JsonComponent
 public class TestSerializer extends JsonSerializer<String> {
 
-    private final String BAHT_SYMBOL = "฿";
+    @Value("${thai.currency.thai-symbol}")
+    private String thaiCurrencySymbol;
 
     @Override
     public void serialize(String stringValue, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         if (stringValue != null) {
-            jsonGenerator.writeString(String.format("%s %s", BAHT_SYMBOL, stringValue));
+            jsonGenerator.writeString(String.format("%s %s", thaiCurrencySymbol, stringValue));
         }
     }
 }
--- src/test/java/com/example/demo/BrazeControllerTest.java
+++ src/test/java/com/example/demo/BrazeControllerTest.java
@@ -1,5 +1,7 @@
-package com.example.demo.controllers;
+package com.example.demo;
 
+import com.example.demo.controllers.UserDataController;
+import com.example.demo.serializer.TestSerializer;
 import com.example.demo.services.BrazeUserDataService;
 import lombok.NoArgsConstructor;
 import org.junit.Before;
@@ -7,7 +9,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,24 +31,23 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@NoArgsConstructor
-@RunWith(MockitoJUnitRunner.class)
-@WebMvcTest(value = UserDataController.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class BrazeControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     private UserDataController userDataController;
 
-    @Mock
+    @MockBean
     private BrazeUserDataService brazeUserDataService;
 
     @Before
     public void setUp() {
         userDataController = new UserDataController(brazeUserDataService);
-
-        mockMvc = MockMvcBuilders.standaloneSetup(this.userDataController).build();
     }
 
     @Test
@@ -40,6 +55,7 @@ public class BrazeControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
                 .get("/user/data/serialize"))
                 .andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.inner.message", is("฿ 1000")));
     }
 }
