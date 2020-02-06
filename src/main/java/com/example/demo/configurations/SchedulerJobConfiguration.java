3b9b9a64f2f9014b0847a10a03bde58692542d4d
Eluzy Demo <eluzy.demo@gmail.com>
2020-02-06 23:21:18 +0700

    learn schedule config and docker volume

--- src/main/java/com/example/demo/configurations/SchedulerJobConfiguration.java
+++ src/main/java/com/example/demo/configurations/SchedulerJobConfiguration.java
@@ -3,7 +3,6 @@ package com.example.demo.configurations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableScheduling
