0ba0d06234e240c708734830240c49ea848abff4
Eluzy Demo <eluzy.demo@gmail.com>
2020-03-07 12:05:39 +0700

    add controller level exception handler

--- src/main/java/com/example/demo/controllers/TestMetricsController.java
+++ src/main/java/com/example/demo/controllers/TestMetricsController.java
@@ -3,7 +3,9 @@ package com.example.demo.controllers;
 import com.example.demo.models.request.TestValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.Valid;
 
@@ -20,5 +22,10 @@ public class TestMetricsController {
     @PostMapping
     public ResponseEntity testCustomValidator(@Valid @RequestBody TestValidator testValidator) {
         return ResponseEntity.ok(testValidator.getStringList());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    private ResponseEntity testExceptionHandler(MethodArgumentNotValidException ex, WebRequest request) {
+        return ResponseEntity.badRequest().body(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
     }
 }
