85e66afe34b4ddcf3645afffed30a64b19efb54e
Ratchasit.Amo <ratchasit.amo@ascendcorp.com>
2020-06-03 21:12:20 +0700

    Add a java functional interface

---
+++ src/main/java/com/example/demo/services/interfaces/JavaFunctionalInterface.java
@@ -0,0 +1,6 @@
+package com.example.demo.services.interfaces;
+
+public interface JavaFunctionalInterface {
+//  java functional interface is a interface that contains only 1 method
+  void onlyThisMethodAvailable(String whatever);
+}
